c0105015b56ab2d21799dc300d1820762de19451
Mosveros <emirhanaydin95@gmail.com>
2016-10-29 05:50:06 -0700

    Program, ebeveyn işlemin yavru işlemin sonlanmasını bekleyeceği şekilde tasarlandı.

--- pro1.c
+++ pro1.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
-  pid_t pid = fork();
+int main(int argc, char **argv) {
+    pid_t pid;
 
-  if (pid == 0) // Çatallama başarılı
+    int n;
+    printf("0'dan n'e kadar olan cift sayilarin toplami icin n degerini giriniz: ");
+    scanf("%d", &n);
+
+    pid = fork(); // Çatallama işlemi yapılır ve oluşan çocuk işlemin pid'i değişkene atanır.
+    if (pid == 0) // Çatallama başarılı
     {
-    }
-  else if (pid < 0) // Çatallama başarısız
+
+    } else if (pid < 0) // Çatallama başarısız
     {
-      printf("fork() basarisiz!\n");
-      return 1;
-    }
-  else // Çatallamadan sonra ebeveyn işlem
+        printf("fork() basarisiz!\n");
+        return 1;
+    } else // Çatallamadan sonra ebeveyn işlem
     {
+        int donusDurumu;
+        wait(&donusDurumu); // Yavru işlem tamamlayıncaya kadar beklenir. Çocuk işlemin
+        // dönüş değeri değişkene aktarılır.
+
+        if (donusDurumu == 0) // Çocuk işlem hatasız bir şekilde sonlandıysa.
+        {
+
+        } else // Hata koduyla sonlanmışsa
+        {
+            printf("Cocuk islem bir hata ile sonlandirildi!\n");
+        }
     }
 
-  return 0;
+    return 0;
 }
